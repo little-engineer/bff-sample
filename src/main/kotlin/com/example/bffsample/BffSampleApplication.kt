ba079c080ec3c60ab64662f73f1f4ec62542f1d5
little <>
2020-08-15 19:33:47 +0900

    add spring boot and kotlin project for BFF sample.

---
+++ src/main/kotlin/com/example/bffsample/BffSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bffsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BffSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<BffSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/bffsample/BffSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bffsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BffSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
