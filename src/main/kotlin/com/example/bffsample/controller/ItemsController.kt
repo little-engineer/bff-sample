d64a370d48d6df580d6d400d0e736ae037f8f88a
little <>
2020-08-16 18:06:26 +0900

    add unit test code for getItem api in controller.

--- src/main/kotlin/com/example/bffsample/controller/ItemsController.kt
+++ src/main/kotlin/com/example/bffsample/controller/ItemsController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/items")
-class ItemListController {
+class ItemsController {
 
     @GetMapping("/{id}")
     @ResponseBody
