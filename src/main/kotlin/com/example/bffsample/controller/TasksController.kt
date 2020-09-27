a4065cf6c20fc55e8be905c18f1d82caeddf1664
little <>
2020-09-27 15:55:22 +0900

    add to call external user api.

--- src/main/kotlin/com/example/bffsample/controller/TasksController.kt
+++ src/main/kotlin/com/example/bffsample/controller/TasksController.kt
@@ -2,13 +2,17 @@ package com.example.bffsample.controller
 
 import com.example.bffsample.model.forfrontend.Task
 import com.example.bffsample.repository.TaskRepository
+import com.example.bffsample.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/tasks")
-class TasksController @Autowired constructor(val taskRepository: TaskRepository){
+class TasksController @Autowired constructor(
+        val taskRepository: TaskRepository,
+        val userRepository: UserRepository
+){
 
     @GetMapping("/{taskId}")
     @ResponseBody
@@ -17,6 +21,9 @@ class TasksController @Autowired constructor(val taskRepository: TaskRepository)
 
         val task = taskRepository.getTask(taskId)
         println(task)
+
+        val user = userRepository.getUser(1)
+        println(user)
 
         return Task(
                 taskId = taskId,
---
+++ src/main/kotlin/com/example/bffsample/model/externalapi/User.kt
@@ -0,0 +1,9 @@
+package com.example.bffsample.model.externalapi
+
+import java.util.*
+
+data class User (
+        val userId: Int? = null,
+        val userName: String? = null,
+        val created: Date? = null
+)
