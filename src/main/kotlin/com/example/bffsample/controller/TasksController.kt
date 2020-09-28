2f1f34a79dd771bfacd8cdb05c467d360ab19ccf
little <>
2020-09-29 08:30:53 +0900

    add service layer for task handling

--- src/main/kotlin/com/example/bffsample/controller/TasksController.kt
+++ src/main/kotlin/com/example/bffsample/controller/TasksController.kt
@@ -1,8 +1,7 @@
 package com.example.bffsample.controller
 
 import com.example.bffsample.model.forfrontend.Task
-import com.example.bffsample.repository.TaskRepository
-import com.example.bffsample.repository.UserRepository
+import com.example.bffsample.service.TaskService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -10,25 +9,13 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/tasks")
 class TasksController @Autowired constructor(
-        val taskRepository: TaskRepository,
-        val userRepository: UserRepository
+        val taskService: TaskService
 ){
 
     @GetMapping("/{taskId}")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
     fun getTask(@PathVariable("taskId") taskId: Int): Task {
-
-        val task = taskRepository.getTask(taskId)
-        println(task)
-
-        val user = userRepository.getUser(1)
-        println(user)
-
-        return Task(
-                taskId = taskId,
-                title = "タスクのタイトル",
-                description = "タスクの詳細説明",
-                userName = "タスク担当者A")
+        return taskService.getTask(taskId)
     }
 }
