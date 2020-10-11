a708097813d0b21434e90d96243ced8b1d5ead3e
little <>
2020-10-11 17:06:33 +0900

    add logging to controller class.

--- src/main/kotlin/com/example/bffsample/controller/TasksController.kt
+++ src/main/kotlin/com/example/bffsample/controller/TasksController.kt
@@ -2,6 +2,7 @@ package com.example.bffsample.controller
 
 import com.example.bffsample.model.forfrontend.Task
 import com.example.bffsample.service.TaskService
+import org.apache.commons.logging.LogFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -11,11 +12,14 @@ import org.springframework.web.bind.annotation.*
 class TasksController @Autowired constructor(
         val taskService: TaskService
 ){
+    private val log = LogFactory.getLog(TasksController::class.java)
 
     @GetMapping("/{taskId}")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
     fun getTask(@PathVariable("taskId") taskId: Int): Task {
+        log.info("getTask is called with taskId($taskId).")
+
         return taskService.getTask(taskId)
     }
 }
