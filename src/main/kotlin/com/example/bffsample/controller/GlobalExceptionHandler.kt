4e1b07af7b535d95c106416fb4bfde095b6211cc
little <>
2021-02-14 14:47:36 +0900

    refactor to remove unnecessary import.

--- src/main/kotlin/com/example/bffsample/controller/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/bffsample/controller/GlobalExceptionHandler.kt
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.client.ResourceAccessException
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
