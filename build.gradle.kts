9e411602864d8f8bc372206a907d73e7a56dc1a1
little <>
2021-02-23 17:51:13 +0900

    add jacoco to gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     kotlin("jvm") version "1.3.72"
     kotlin("plugin.spring") version "1.3.72"
+    id("jacoco")
 }
 
 group = "com.example"
